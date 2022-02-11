ddce3829b159c215f478985473f701d2198bd1b4
tboonma <tboonma@hotmail.com>
2022-02-11 21:09:14 +0700

    Finish retriving data from API

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,11 +1,12 @@
-import React from "react"
+import axios from 'axios'
+import React from 'react'
 
 const Recipes = () => {
   return (
     <div className="recipes">
       <form className="searchbar">
         <input type="search" placeholder="Search for Meal" />
-        <button className="inputbutton">Search</button>
+        <button className="inputbutton" onclick>Search</button>
       </form>
       <div className="menulist">
         <div className="menu">
@@ -22,6 +23,30 @@ const Recipes = () => {
       </div>
     </div>
   )
+}
+
+async function getFoodListFromApi(keyword) {
+  const response = await axios.get(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`
+  )
+  return response.data
+}
+
+async function getFoodByIdFromApi(id) {
+  const response = await axios.get(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  )
+  return response.data
+}
+
+function getMealList(keyword) {
+  const rawData = getFoodListFromApi(keyword)
+  return rawData.meals
+}
+
+function getMealDetail(id) {
+  const rawData = getFoodByIdFromApi(id)
+  return rawData.meals[0]
 }
 
 export default Recipes
